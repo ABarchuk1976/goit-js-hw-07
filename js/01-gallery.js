1298a2516c63d9f7d09d3a6a0012c0aa04c5eb85
Barchuk Andrii <abarchuk1976@gmail.com>
2022-09-04 00:01:09 +0300

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+let markup = '';
+
+for (const img of galleryItems) {
+  markup += `<div class="gallery__item">
+		<a class="gallery__link" href="${img.original} title="${img.description}">
+			<img class="gallery__image"
+				src=${img.preview} 
+				data-source=${img.original} 
+				alt="${img.description}" 
+			/>
+		</a>
+	</div>`;
+}
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+galleryRef.addEventListener('click', (event) => {
+  if (event.target.tagName !== 'IMG') return;
+
+  event.preventDefault();
+
+  const instance = basicLightbox.create(
+    `<img src=${event.target.dataset.source} width="80%">`
+  );
+
+  instance.show();
+
+  document.addEventListener('keydown', (eventKeyboard) => {
+    console.log(eventKeyboard.key);
+    if (eventKeyboard.key === 'Escape') instance.close();
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
+import SimpleLightbox from '../node_modules/simplelightbox/src/simple-lightbox.js';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+let markup = '';
 
-console.log(galleryItems);
+for (const img of galleryItems) {
+  markup += `<a class="gallery__item" href=${img.original} title="${img.description}">
+			<img 
+				class="gallery__image"
+				src=${img.preview} 
+				alt="${img.description}"
+			/>
+			</a>`;
+}
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+galleryRef.addEventListener('click', (event) => {
+  if (event.target.tagName !== 'IMG') return;
+
+  event.preventDefault();
+
+  const lightbox = new SimpleLightbox('.gallery__item');
+
+  document.addEventListener('keydown', (eventKeyboard) => {
+    if (eventKeyboard.key === 'Escape') lightbox.close();
+  });
+});
